2ab09d5d6474f64033d04b894bc4665062346ac7
ta-chibana <tachibana.gmc@gmail.com>
2017-01-08 21:44:34 +0900

    Add save contents api

---
+++ app/controllers/api/page_controller.rb
@@ -0,0 +1,28 @@
+class Api::PageController < ApplicationController
+  before_action :validate_params
+
+  def save
+    user = User.find_or_create_by(name: params[:user])
+    page = Page.find_or_create_by(user: user, name: params[:page])
+    part = Part.find_or_initialize_by(page: page)
+    part.contents = params[:contents]
+
+    binding.pry
+    if part.save
+      head :ok
+    else
+      failed
+    end
+  end
+
+  private
+
+  def validate_params
+    failed if params[:user].blank?
+    failed if params[:page].blank?
+  end
+
+  def failed
+    head :bad_request
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :parts
   resources :pages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    post :page, controller: :page, action: :save
+  end
 end
